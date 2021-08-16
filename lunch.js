92467ace33eaae1637cd057bcaf0b78651090220
shaun <smccreight154@gmail.com>
2021-08-16 19:12:49 +0000

    a way to choose lunch

--- lunch.js
+++ lunch.js
@@ -1,27 +1,23 @@
-/*
- * Modify the contents of the function below, such that:
- *
- * If we're not hungry, we want to tell ourselves to get back to work.
- * Otherwise, we want to pick something up and eat it in the lab when
- * we've got less than 20 minutes or to try a place nearby if we've
- * got between 20 and 30 minutes. If we have any more time than that,
- * we want to remind ourselves that we're in a bootcamp and that we
- * should reconsider how much time we actually have to spare.
- *
- * hungry is a Boolean, representing if you're hungry or not.
- * availableTime is a Number representing the time you have for lunch,
- * in minutes.
- */
+let whatToDoForLunch = function (hungry, availableTime) {
+  if (hungry === false) {
+    console.log("I'm not hungry.");
 
-const whatToDoForLunch = function(hungry, availableTime) {
-  console.log("I don't know what to do!");
+  } else {
+    if (availableTime === 20) {
+      console.log("Make a sandwich.");
+    } else if (availableTime >= 50) {
+      console.log("Make some chicken and rice.");
+    } else if (availableTime === 30) {
+      console.log("Make some soup.");
+    } else if (availableTime <= 15) {
+      console.log("Make some instant noodles.");
+    }
+  }
 }
 
 
-/*
- * This is some test runner code that's simply calling our whatToDoForLunch function
- * defined above to verify we're making the right decisions. Do not modify it!
- */
+
+
 
 console.log("I'm hungry and I have 20 minutes for lunch.");
 whatToDoForLunch(true, 20);
@@ -37,3 +33,4 @@ console.log("---");
 
 console.log("I'm hungry and I have 15 minutes for lunch.");
 whatToDoForLunch(true, 15);
+
